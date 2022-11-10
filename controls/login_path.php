9fffc963caa5ebb4993ec9d4ea8f27ecc7e6bf23
farah-larbah <farahlarbah@gmail.com>
2022-11-10 18:20:46 +0200

    login page is ready

--- controls/login_path.php
+++ controls/login_path.php
@@ -1,2 +1,35 @@
 <?php
 include(MAIN_PATH. "/database/db.php");
+
+$fullname="";
+$errors=array();
+
+if (isset($_POST['submit'])) {
+      global $conn;
+      $fullname = mysqli_real_escape_string($conn,$_POST['username']);
+      $password = mysqli_real_escape_string($conn,$_POST['pass']);
+
+       // Ensuring that the user has not left any input field blank
+      // error messages will be displayed for every blank input
+if (empty($fullname)) { array_push($errors, "Full name is required"); }
+if (empty($password)) { array_push($errors, "Password is required"); }
+
+
+// Checking for the errors
+if (count($errors) == 0) {
+$query = "SELECT full_name,password  FROM `user` WHERE full_name='".$fullname."' and password='".$password."'";
+$results = mysqli_query($conn, $query);
+// $results = 1 means that one user with the entered full name exists
+if (mysqli_num_rows($results) == 1) {
+   
+    $_SESSION['fullname'] = $fullname;
+  // page after logging in
+  header('Location: UI/group/main page for group.php');
+}
+else {
+   
+  // If the full name and password are not in database
+  array_push($errors," Full name or password incorrect");
+}
+}
+}
--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 include("path.php"); 
 include(MAIN_PATH."/controls/login_path.php");
 ?>
@@ -10,13 +11,14 @@ include(MAIN_PATH."/controls/login_path.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS\login_and_singup.css" type="text/css"/>
-    <script src="JavaScript\check.js"></script> 
+    <script src="JavaScript\check.js"></script>
+    <!--icon8-->
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <title>login</title>
 
   
 </head>
 <body>
-   
    
     <div class="login-container">
     <div class="circle-container"> 
@@ -24,52 +26,33 @@ include(MAIN_PATH."/controls/login_path.php");
     <img class="imagelogo" id="image" src="sources\image\singup-image.png" alt="no image"/>
     </div>
         <p class="main_text"><b>Welcome</b><br>Back! </p>
-        <form method="post" action="" class="login-form" >
+        <form method="post" action="" class="login-form"  onsubmit="return check_Enter(this)">
+
+       
             <div class="form-field ">
-                <input id="username" name="username" type="text"  placeholder="Full Name" size="27" maxlength="30"/>
-                <div class="error"></div>
+                <input id="username" name="username" type="text"  placeholder="Full Name" size="27" maxlength="30"  value="<?php echo $fullname;?>"/>
             </div>
               
             <div class="form-field">
-                <input id="pass" name="pass" type="password" placeholder="Password" maxlength="25"/> 
-                <div class="error"></div> 
+                <input id="pass" name="pass" type="password" placeholder="Password" maxlength="25" /> 
             </div>
+
+
+          <?php if(count($errors)> 0): ?>
+                    <div class="error" style="color: #D92A2A;padding_right=30px "> 
+                     <?php foreach($errors as $error): ?>
+                        <li style="list-style-type: none; " ><i class="las la-exclamation-circle" style="color: #D92A2A;font-weight: 600; font-size: 16px;"></i><?php echo($error); ?></li>
+                     <?php endforeach; ?>
+                    </div> 
+          <?php endif; ?> 
                 
-            <button type="submit" id="submit" name="submit" onclick="check_Enter()">Login</button>
+         <button type="submit" id="submit" name="submit">Login</button>
         </form>
         <div class="additional-action">
             <p>Create an account <b><u>Singup</u></b></p>
         </div>
     </div>
     <form>
-
-        <?php 
-          if(isset($_POST['submit'])){
-            global $conn;
-                $fullname = mysqli_real_escape_string($conn,$_POST['username']);
-                $password = mysqli_real_escape_string($conn,$_POST['pass']);
-            
-                if ($fullname != "" && $password != ""){
-
-                    $sql_query = "SELECT full_name,password  FROM `user` WHERE full_name='".$fullname."' and password='".$password."'";
-                    $result = mysqli_query($conn,$sql_query);
-                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-                    $count = mysqli_num_rows($result);  
-                    
-                    if($count == 1){
-                        $_SESSION['fullname'] = $fullname; 
-                         header('Location: UI/group/main page for group.php'); 
-                    }else{
-                        header('Location: login.php');
-                        $message_error = "The username or password is wrong!";
-                    }
-            
-                }
-            
-            }
-        
-        ?>
-    
 
 </body>
 </html>
