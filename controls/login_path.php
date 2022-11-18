44166179c893f4cffb27703549ad9e1fbb624a09
farah-larbah <farahlarbah@gmail.com>
2022-11-18 21:58:51 +0200

    updating password in login ,full name for admin signup

--- controls/login_path.php
+++ controls/login_path.php
@@ -10,12 +10,10 @@ if (isset($_POST['submit'])) {
 
     
       global $conn;
-      /* md5('somerandomtextthatyouknow'.$_POST['password']) */
+      
       $fullname = trim(mysqli_real_escape_string($conn,$_POST['username']));
       $password = mysqli_real_escape_string($conn,$_POST['password']);
-     
-      
-      /* $user_id = mysqli_real_escape_string($conn,$_POST['user_id']); */
+    
   
   // Ensuring that the user has not left any input field blank
   // error messages will be displayed for every blank input
@@ -25,36 +23,43 @@ if (empty($password)) { array_push($errors, "Password is required"); }
 
 // Checking for the errors
 if (count($errors) == 0) {
-$query = "SELECT user_id,full_name,password,admin  FROM `user` WHERE full_name='".$fullname."' and password='".$password."'";
+$query = "SELECT user_id,full_name,password,u_img,admin  FROM `user` WHERE full_name='".$fullname."'";
 $results = mysqli_query($conn, $query);
+
+
 
 // $results = 1 means that one user with the entered full name exists
 if (mysqli_num_rows($results) == 1) 
  {
-  while ($row =mysqli_fetch_assoc($results))
+   while ($row =mysqli_fetch_assoc($results))
   {
-    if($row["admin"]=="1"){
-
+    if($fullname && password_verify($password,$row['password'])){
+    if($row["admin"]=="1")
+    {
       $_SESSION['full_name'] = $fullname;
-      $_SESSION['id'] = $user_id;
+      $_SESSION['user_id'] =$row['user_id'];
+      
       header('Location: UI/control_panel/groups_control_panel.php');
       $conn->close();
       exit();
-    }
+     }
     else {
       $_SESSION['full_name'] = $fullname;
+      /* $_SESSION['u_img'] =$row['u_img']; */
       // page after logging in
       header('Location: UI/group/main page for group.php');
       $conn->close();
-     exit();
+      exit();
      }
-   }
- } 
+   
+   } array_push($errors," password incorrect"); }
+   
+}
 
 else {   
   // If the full name and password are not in database
-array_push($errors," Full name or password incorrect");
+array_push($errors," Full name or password incorrect");}
 
 }
 }
-}
+
--- login.php
+++ login.php
@@ -39,9 +39,9 @@ include(MAIN_PATH."/controls/login_path.php");
                 
 
           <?php if(count($errors)> 0): ?>
-                    <div class="error" style="color: #D92A2A;padding_right=30px;margin-bottom: 20px; "> 
+                    <div class="error" style="color: #D92A2A;margin-bottom:20px;font-size: 15px; "> 
                      <?php foreach($errors as $error): ?>
-                        <li style="list-style-type: none; " ><i class="las la-exclamation-circle" style="color: #D92A2A;font-weight: 600; font-size: 16px;"></i><?php echo($error); ?></li>
+                        <li style="list-style-type: none; " ><i class="las la-exclamation-circle" style="color: #D92A2A;font-weight: 600; font-size: 16px;padding_right=90px"></i><?php echo($error); ?></li>
                      <?php endforeach; ?>
                     </div> 
           <?php endif; ?> 
