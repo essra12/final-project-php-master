1715053ae29ced8b0b3c7127dc36df7de6739a85
farah-larbah <farahlarbah@gmail.com>
2022-11-20 23:24:14 +0200

    ........

---
+++ UI/student/materials.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/materrials_Assignments.css">
+    <title>Add Materials</title>
+    
+</head>
+<body>
+<div class="header-div">
+            
+           <form class="example"  method="POST" action="" onsubmit="return check_Enter(this)">
+           <div class="full_name">
+           <h1>Materials </h1>
+           </div>
+         
+           </form> 
+          
+           </div>
+       </div>
+</body>
+</html>
