9b863d942b86c7fc645cbaf39698cf3f68bf762e
farah-larbah <farahlarbah@gmail.com>
2022-11-26 00:58:05 +0200

    inside_group starting to work on it

--- UI/group/inside_group.php
+++ UI/group/inside_group.php
@@ -1,2 +1,79 @@
-<?php
-echo "you are inside group page";
+
+<?php 
+include("../../path.php"); 
+include(MAIN_PATH."/controls/main_group_page.php");
+$user_id=$_SESSION['user_id'];
+
+
+?>
+<!DOCTYPE html>
+<head>
+    <title>Group</title>
+    <meta name="descreption " content=" " />
+    <link rel="stylesheet" href="../../css/main_group_page.css">
+    <!--icon8-->
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    </head>
+<html>
+<body>
+       <!-- header div-->
+       <div class="header-div">
+            
+             <h5 id="date" style="font-size:24px; padding-bottom: 10px; padding-left:14px;">  </h5>
+         
+        </div>
+           
+         <!-- ************************************************************************************* -->
+
+         <main>
+       <!--  group cards -->
+        <div class="group-cards">
+            <a href="">
+                <div class="group-card">
+                    <div class="group-card-info">
+                        <h3>Group name...</h3>
+                        
+                    </div>
+                </div>
+            </a>
+
+            <a href="">
+                <div class="group-card">
+                    <div class="group-card-info">
+                        <h3>Group name...</h3>
+                        
+                    </div>
+                </div>
+            </a>
+
+           <a href="">
+                <div class="group-card">
+                    <div class="group-card-info">
+                        <h3>Group name...</h3>
+                        
+                    </div>
+                </div>
+            </a>
+
+            <a href="">
+                <div class="group-card">
+                    <div class="group-card-info">
+                        <h3>Group name...</h3>
+                     </div>
+                </div>
+            </a>
+
+         
+
+         
+            
+        </div>
+    </main>
+
+<!-- java script for current date -->
+    <script>
+       
+    </script>
+      
+</body>
+</html>
