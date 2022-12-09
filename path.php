59cd2860abeaf3d3ad19f62d87e477db26b443c5
Essra Sowan <essrasowan711@gmail.com>
2022-12-09 12:50:02 +0200

    assiments

--- path.php
+++ path.php
@@ -1,4 +1,4 @@
 <?php 
 /* متغير لحفظ مسار المجلد الاساسي */
 define("MAIN_PATH",realpath(dirname(__FILE__)));
-define("BASE_URL","http://localhost:8000/final/final-project-php-master");
+define("BASE_URL","http://localhost/final-project-php-master");
