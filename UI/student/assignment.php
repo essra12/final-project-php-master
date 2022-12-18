7f446298cdd4ad801002bd5d1c9b1b6ce48c00bc
Yakeen-Zreba <yakeenzreba@gmail.com>
2022-12-18 12:53:49 +0200

    new

--- UI/student/assignment.php
+++ UI/student/assignment.php
@@ -10,7 +10,7 @@ $files=infoforstudent();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="../../css/materials.css"> 
+     <link rel="stylesheet" href="../../css/material.css"> 
        <!--icon8-->
        <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
       <title>Assignments</title>
