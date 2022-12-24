bf4361c5427702f2194cdc0e84344a9e308b0ac3
Essra Sowan <essrasowan711@gmail.com>
2022-12-24 18:40:12 +0200

    profiles

--- UI/teacher/profile teacher.php
+++ UI/teacher/profile teacher.php
@@ -1,100 +1,53 @@
-
-<?php
+<?php 
 include("../../Database/db.php");
+include("../../controls/profiles.php");
+ $user_id=$_SESSION['user_id'];
 ?>
+
 <!DOCTYPE html>
 <head>
-    <title>teacher-profile</title>
+    <title>student-profile</title>
     <meta name="descreption " content=" " />
-    <link rel="stylesheet" href="..\..\CSS\profiless.css" />
+    <link rel="stylesheet" href="../../CSS/profile.css"/>
      <!--icons-->
      <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <script src="https://kit.fontawesome.com/e1ca29be31.js" crossorigin="anonymous"></script>
+    
     </head>
-
     <!--  style for profile student -->
 <style>
- @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
-*{
-  font-family: "Poppins", sans-serif;
-}
-.photo{
-    border-radius: 100px;
-   
-}
-
 </style>
-    <html>
-        <body>
-            <div class="div-photo">
-            <form method="get" action="edit teacher.php">
-<!-- select image for teacher -->
+<html>
+
+
+<body>
+<form method="get" action="edit profile.php" enctype="multipart/form-data">
+<div class="container">
+        <!-- container one for photo,groups,assinament data -->
+    <div class="divphoto">
+            <span id="group" class="las la-user-friends"></span>
+            <span  id="file" style="margin-left: 15%;" class="lar la-file"></span>
+            <span id="id" style="margin-left: 30%;" class="las la-id-card"></span>
+            <!-- php code  -->
             <?php
-             global $conn;
-             $id=$_SESSION['user_id'];/** من صفحة تسجيل الدخول  stud_id*/
-
-                        $sqln="SELECT user.* ,teacher.tr_phone_no ,teacher.tr_id 	
-                        from user ,teacher 
-                        WHERE user.user_id=teacher.user_id  and user.user_id='".$id."';";
-                        /** وتخزينها في متغيرات DB  احضار بيانات من  */                      
-                        $result= mysqli_query($conn,$sqln);
-                        $row =mysqli_fetch_row($result);
-                         $img=$row[3];
-
-   echo " <img class='photo' src='../../sources/image/$img' /> ";
-   ?>
-            <!-- icons -->
-            </div>
-            <div class="div-data">
-            
-
-                <i id="img1" style="font-size: 27px;" class="las la-id-card"></i>
-                <i id="img2" style="font-size: 27px;" class="las la-user"></i>
-                <i id="img3" style="font-size: 27px;" class="las la-phone"></i>
-<!-- select data for taecher  -->
-             <?php
-                        global $conn;
-                        $id=$_SESSION['user_id'];/** من صفحة تسجيل الدخول  stud_id*/
-                        $id3=$_SESSION['passT'];/** login path كلمة السر غسر مشفرة تم احضارها من  */
-                        $sqln="SELECT user.* ,teacher.tr_phone_no ,teacher.tr_id 	
-                        from user ,teacher 
-                        WHERE user.user_id=teacher.user_id and user.user_id='".$id."';";
-                        /** وتخزينها في متغيرات DB  احضار بيانات من  */                      
-                        $result= mysqli_query($conn,$sqln);
-                        $row =mysqli_fetch_row($result);
-                        $id=$row[6];
-                        $name=$row[1];
-                        $phone=$row[5];
-                        $pass=$row[2];
-                       
-                            /** BD عرض البيانات التي تم احضارها من  */
-                         echo " <lable class='l1'>". $id. "</lable>. <lable class='l2'>" .$name ."</lable>" . "<lable class='l3'>" .$phone ."</lable>"  ;
-                         /** SESSION نقل البيانات الي صحة اخري باستخدام  */
-                            
-                            $_GET['name']=$name;
-                            $_GET['phone']=$phone;
-                            $_GET['password']=$pass;
-                            $_GET['password1']=$id3;/**كلمة مرور غير مشفرة  */
-                            $_GET['img']= $img;
-                            $_GET['id']=$id;
-
-
-
-                           $_SESSION['name']=$_GET['name'];
-                           $_SESSION['phone']=$_GET['phone'];
-                           $_SESSION['pass']=$_GET['password'];   
-                           $_SESSION['pass2']= $_GET['password1'];
-                           $_SESSION['img1']=$_GET['img'];
-                           $_SESSION['tid']=$_GET['id'];
-                          
-               
-                  ?>   
-                       
-                <a href="edit teaher.php" >
-                <input name="bts" class="bt1"  type="button" value="Edit"/></a>
-                <a href="..\..\logout.php">
-                <input class="bt2"  name="edit" type="button" value="Logout"/></a>
-                </form>
-            </div>
-           </body>
-           </html>
+             imgteacher();
+            countgroupteacher();
+                         ?>
+    </div>
+<!-- ----------------------------- container two ------------------- -->
+    <div class="divdata" >
+    <label class="name">Name :</label>
+    <label class="spe" >Phone Number :</label>
+            <!-- php code  -->
+            <?php
+            dataforteacher();
+            ?>
+            <a href="edit teaher.php" >
+            <input name="bts" class="bt1"  type="button" value="Edit"/></a>
+            <a href="..\..\logout.php">
+            <input class="bt2"  name="edit" type="button" value="Logout"/></a>
+    </div>
+</div>
+</form>
+</body>
+</html>
--- controls/profiles.php
+++ controls/profiles.php
@@ -1,5 +1,8 @@
 
 <?php
+/**-------------------------------------------------Student ----------------------------------------
+ * ---------------------------------------------------------------------------------------------------
+ * --------------------------------------------------------------------------------- */
 function stprofileimg(){
                         global $conn;
                       /*  $name=$_SESSION['full_name'];*/
@@ -12,50 +15,166 @@ function stprofileimg(){
                         $img=$row[3];
                         echo " <img class='photo' src='../../sources/image/$img' /> ";    
                         
-}
-                        
-                      function informationPR(){  
-
+}               
+                      function informationstudent(){  
                         global $conn;
-                        /*   $password=$_SESSION['password'];*/
-                          
-                          /* $name=$_SESSION['full_name'];*/
-                            $id=$_SESSION['user_id'];/** من صفحة تسجيل الدخول  stud_id*/
-                            $id3=$_SESSION['pass4'];/** login path كلمة السر غسر مشفرة تم احضارها من  */
-                           $sql="SELECT USER.* ,student.stu_id,student.stu_specialization 
-                           FROM user,student
-                           WHERE user.user_id=student.user_id and user.user_id='".$id."';";
-                           $result= mysqli_query($conn,$sql);
-                           $row =mysqli_fetch_row($result);
-                           $id=$row[5];
-                           $name=$row[1];
-                           $spe=$row[6];
-                           $pass=$row[2];
-                           $img=$row[3];
-                 
-                 /*          
-                  if(password_verify($id3,$pass)){
-                  $test=$id3;
-                   echo $test;
-   
-                  }*/
-                           /** عرض البيانات التي تم احضارها من قاعدة البيانات  */
-                           echo "<lable class='l1'>".$id."</lable>"."<lable class='l2'>".$name."</lable>"."<lable class='l3'>".$spe."</lable>" ;
-                               /** session  باستخدام  edit profile  ارسال البيانات الي صفحة  */
-                               $_GET['id']=$id;           
-                               $_GET['name']=$name;
-                               $_GET['spe']=$spe;
-                               $_GET['password']=$pass;/**كلمة مرور مشفرة */
-                               $_GET['password1']=$id3;/**كلمة مرور غير مشفرة  */
-                               $_GET['img']= $img;
-   
-                                
-                              $_SESSION['id']=$_GET['id'];
-                              $_SESSION['name']=$_GET['name'];
-                              $_SESSION['spe']=$_GET['spe'];
-                              $_SESSION['pass']=$_GET['password']; 
-                              $_SESSION['pass2']= $_GET['password1'];
-                              $_SESSION['img1']=$_GET['img'];
-   
+                         /* $name=$_SESSION['full_name'];*/
+                        $id=$_SESSION['user_id'];/** من صفحة تسجيل الدخول  stud_id*/
+                        $id3=$_SESSION['pass4'];/** login path كلمة السر غسر مشفرة تم احضارها من  */
+                       $sql="SELECT USER.* ,student.stu_id,student.stu_specialization 
+                       FROM user,student
+                       WHERE user.user_id=student.user_id and user.user_id='".$id."';";
+                       $result= mysqli_query($conn,$sql);
+                       $row =mysqli_fetch_row($result);
+                       $id=$row[5];
+                       $name=$row[1];
+                       $spe=$row[6];
+                       $pass=$row[2];
+                       $img=$row[3];
+                     echo"  <label class='LabelID' style='margin-left: 30%;'>ID <br>$id</label>";
+                     echo"   <label class='stname' >$name</label>";
+                     echo"   <label class='stspe' >$spe</label>";
+      
+                     $_GET['id']=$id;           
+                     $_GET['name']=$name;
+                     $_GET['spe']=$spe;
+                     $_GET['password']=$pass;/**كلمة مرور مشفرة */
+                     $_GET['password1']=$id3;/**كلمة مرور غير مشفرة  */
+                     $_GET['img']= $img;
+      
+                      
+                    $_SESSION['id']=$_GET['id'];
+                    $_SESSION['name']=$_GET['name'];
+                    $_SESSION['spe']=$_GET['spe'];
+                    $_SESSION['pass']=$_GET['password']; 
+                    $_SESSION['pass2']= $_GET['password1'];
+                    $_SESSION['img1']=$_GET['img'];
                 }
-                        ?>
+                function countgroupandassinament(){
+                  global $conn;
+                  $id=$_SESSION['id'];
+                  $query="SELECT COUNT(student_group.g_no)  FROM student_group WHERE student_group.stu_id='$id' ";
+                   $result = $conn->query($query);
+                   if ($result->num_rows > 0) {
+                  while($row = $result->fetch_assoc()) {
+                  $idq=$row['COUNT(student_group.g_no)'];}}
+                  echo"  <label class='Lablegroup'>Groups<br>&nbsp&nbsp&nbsp&nbsp&nbsp$idq</label>";
+                 /** الاستعلام عن عدد الفروض الذي قام بإرسالها  الطالب  */
+                  $sqlass="SELECT COUNT(post.p_no) from student_group,post,file WHERE student_group.stu_group=post.stu_group and post.p_no=file.p_no and student_group.stu_id='$id' ";
+                  $result = $conn->query($sqlass);
+                  if ($result->num_rows > 0) {
+                 while($row = $result->fetch_assoc()) {
+                 $idq=$row['COUNT(post.p_no)'];}}
+                 echo"  <label class='LabelAssinaments' style='margin-left: 13%;'>Assinaments<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp$idq</label>";
+                  
+                }
+
+/**---------------------------------------------Teacher-----------------------------------------------------
+ * -------------------------------------------------------------------------------------------------------------------------------
+ */
+
+function imgteacher(){
+
+   global $conn;
+   $id=$_SESSION['user_id'];/** من صفحة تسجيل الدخول  stud_id*/
+   $sqln="SELECT user.* ,teacher.tr_phone_no ,teacher.tr_id 	
+   from user ,teacher 
+   WHERE user.user_id=teacher.user_id  and user.user_id='".$id."';";
+   /** وتخزينها في متغيرات DB  احضار بيانات من  */                      
+   $result= mysqli_query($conn,$sqln);
+   $row =mysqli_fetch_row($result);
+    $img=$row[3];
+    echo " <img class='photo' src='../../sources/image/$img' /> ";
+}
+ function countgroupteacher(){
+                  $user_id=$_SESSION['user_id'];
+                  global $conn;
+                  $query="SELECT COUNT(groups.g_no) FROM groups,user,teacher WHERE user.user_id=teacher.user_id and teacher.tr_id=groups.tr_id and user.user_id='$user_id'";
+                   $result = $conn->query($query);
+                   if ($result->num_rows > 0) {
+                  while($row = $result->fetch_assoc()) {
+                  $idq=$row['COUNT(groups.g_no)'];}}
+                  echo"  <label class='Lablegroup'>Groups<br>&nbsp&nbsp&nbsp&nbsp&nbsp$idq</label>";
+                  /** الاستعلام عن عدد الفروض الذي قام بإرسالها  الاستاذ  */
+                  $sqlass="SELECT  COUNT(announcement.an_no) FROM user,teacher,groups,announcement WHERE user.user_id=teacher.user_id and teacher.tr_id=groups.tr_id and groups.g_no=announcement.g_no and user.user_id='$user_id'";
+                  $result = $conn->query($sqlass);
+                  if ($result->num_rows > 0) {
+                  while($row = $result->fetch_assoc()) {
+                  $idq=$row['COUNT(announcement.an_no)'];}}
+                  echo"  <label class='LabelAssinaments' style='margin-left: 13%;'>announcement<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp$idq</label>";      
+                }
+                function dataforteacher(){
+                  global $conn;
+                  $id=$_SESSION['user_id'];/** من صفحة تسجيل الدخول  stud_id*/
+                  $id3=$_SESSION['passT'];/** login path كلمة السر غسر مشفرة تم احضارها من  */
+                  $sqln="SELECT user.* ,teacher.tr_phone_no ,teacher.tr_id 	
+                  from user ,teacher 
+                  WHERE user.user_id=teacher.user_id and user.user_id='".$id."';";
+                  /** وتخزينها في متغيرات DB  احضار بيانات من  */                      
+                  $result= mysqli_query($conn,$sqln);
+                  $row =mysqli_fetch_row($result);
+                  $id=$row[6];
+                  $name=$row[1];
+                  $phone=$row[5];
+                  $pass=$row[2];
+                  $img=$row[3];
+
+     
+                    echo"  <label class='LabelID' style='margin-left: 30%;'>ID <br>$id</label>";
+                    echo"   <label class='stname' >$name</label>";
+                    echo"   <label class='stspe' >$phone</label>";
+
+     
+                    $_GET['name']=$name;
+                                 $_GET['phone']=$phone;
+                                 $_GET['password']=$pass;
+                                 $_GET['password1']=$id3;/**كلمة مرور غير مشفرة  */
+                                 $_GET['img']= $img;
+                                 $_GET['id']=$id;
+     
+                                $_SESSION['name']=$_GET['name'];
+                                $_SESSION['phone']=$_GET['phone'];
+                                $_SESSION['pass']=$_GET['password'];   
+                                $_SESSION['pass2']= $_GET['password1'];
+                                $_SESSION['img1']=$_GET['img'];
+                                $_SESSION['tid']=$_GET['id'];         
+                }
+
+/**---------------------------------------------Admin -----------------------------------------------------
+ * -------------------------------------------------------------------------------------------------------------------------------
+ */
+
+
+ function imgadmin(){
+  global $conn;
+  $id=$_SESSION['user_id'];/** من صفحة تسجيل الدخول  stud_id*/
+  $sqln="  SELECT  `u_img` FROM `user` WHERE `role`='admin' and user.user_id='".$id."';";
+    $result= mysqli_query($conn,$sqln);
+    $row =mysqli_fetch_row($result);
+    $img=$row[0];
+    echo " <img class='photo' src='../../sources/image/$img' /> ";
+ }
+
+function admingroupcount(){
+  $user_id=$_SESSION['user_id'];
+  global $conn;
+  $query="SELECT COUNT(groups.g_no) FROM `groups` ";
+    $result = $conn->query($query);
+   if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+  $idq=$row['COUNT(groups.g_no)'];}}
+  echo"  <label class='Lablegroup'>Groups<br>&nbsp&nbsp&nbsp&nbsp&nbsp$idq</label>";
+}
+
+
+function countTeacherforadminpage(){
+ 
+  global $conn;
+  $query=" SELECT COUNT(teacher.tr_id) FROM `teacher`  ";
+  $result = $conn->query($query);
+  if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+  $idr=$row['COUNT(teacher.tr_id)'];}}
+  echo "<label class='LabelAssinaments' style='margin-left: 15%;'>Teachers<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp$idr</label>  ";
+}
+                ?>
