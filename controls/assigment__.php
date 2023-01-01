57bcf8d2d267c90b0fc6837918085068d248fe95
farah-larbah <farahlarbah@gmail.com>
2023-01-01 14:14:56 +0200

    new

--- controls/assigment__.php
+++ controls/assigment__.php
@@ -36,5 +36,30 @@ $pre->execute();
 $records=$pre->get_result()->fetch_all(MYSQLI_ASSOC);
 return $records;
 }
+function search($stu_id){ 
+    $search="";
+    global $errors;
+    global $conn; 
+
+        $sql="SELECT post.stu_group,user.full_name,user.u_img,student.stu_id FROM post,student_group,announcement,user,student WHERE  post.stu_group=student_group.stu_group AND user.user_id=student.user_id 
+        AND student.stu_id=student_group.stu_id  AND student.stu_id='$stu_id'; ";
+        $pre=$conn->prepare($sql);
+        $pre->execute();
+        $exisiting_student_search=$pre->get_result()->fetch_all(MYSQLI_ASSOC);
+            
+        if($exisiting_student_search)
+        {
+            $conn->close();
+            return $exisiting_student_search;
+        }
+        elseif(!$exisiting_student_search) 
+          {
+            array_push($errors," This student dosn't exist");
+            $search="";
+            return $exisiting_student_search;
+         } 
+                   
+    } 
+
 ?>
 
