e8af065ed630c9f9a0109850c05c71f7f76d06e0
Essra Sowan <essrasowan711@gmail.com>
2023-01-03 19:32:27 +0200

    edit

--- controls/Edit-TeacherPH.php
+++ controls/Edit-TeacherPH.php
@@ -47,18 +47,29 @@ if(count($errors)==0){
     /** حفظ القيم المدخلة في المتغيرات */
     $username = $_POST['name'];
     $phone = $p_new;
+    $userp1 =$_POST['pass'];/**edit profile page  داخل  password  القيمة المدخلة في حقل  */
+    $userp2 =$_POST['cof-pass'];/**edit profile page  داخل  confirm_password  القيمة المدخلة في حقل  */
+
     $userpass1=password_hash($_POST['pass'], PASSWORD_DEFAULT);//password عمل تشفير لل
     $userpass2=password_hash($_POST['cof-pass'], PASSWORD_DEFAULT);//password عمل تشفير لل
     $img=$_POST['u_img'];
 
    /** empty التحقق من حقول الادخال باستحدام  */
- 
-      if(empty($username)) 
-    {
-        $error="* please enter your name   "; 
-    }
-    else 
-    {
+    if(empty($username)) 
+   {   
+       $error="* please enter  your name    "; 
+   }else if(empty($phone)) 
+   {   
+       $error="* please enter  your phone   "; 
+   }else if(empty($userp1)) 
+   {   
+       $error="* please enter  your password  "; 
+   }else if(empty($userp2))
+   {   
+       $error="* please enter  your password again  "; 
+   }
+   else
+   {
         $sqln="UPDATE user,teacher set   user.full_name='$username', user.password='$userpass1',teacher.tr_phone_no='$phone',user.u_img='$img'
         WHERE user.user_id=teacher.user_id and teacher.tr_phone_no='$phon' ;";
         if(mysqli_query($conn,$sqln)){
@@ -98,18 +109,32 @@ if(count($errors)==0){
         /** حفظ القيم المدخلة في المتغيرات */
         $username = $_POST['name'];
         $phone = $p_new;
+        $userp1 =$_POST['pass'];/**edit profile page  داخل  password  القيمة المدخلة في حقل  */
+        $userp2 =$_POST['cof-pass'];/**edit profile page  داخل  confirm_password  القيمة المدخلة في حقل  */
         $userpass1=password_hash($_POST['pass'], PASSWORD_DEFAULT);//password عمل تشفير لل
         $userpass2=password_hash($_POST['cof-pass'], PASSWORD_DEFAULT);//password عمل تشفير لل
         $img=$_POST['u_img'];
     
        /** empty التحقق من حقول الادخال باستحدام  */
-     
-          if(empty($username)) 
-        {
-            $error="* please enter your name   "; 
-        }
-        else 
-        {
+      /** empty التحقق من حقول الادخال باستحدام  */
+    if(empty($username)) 
+   {   
+       $error="* please enter  your name  jj "; 
+   }else if(empty($phone)) 
+   {   
+       $error="* please enter  your phone   "; 
+   }else if(empty($userp1)) 
+   {   
+       $error="* please enter  your password  "; 
+   }else if(empty($userp2))
+   {   
+       $error="* please enter  your password again  "; 
+   }else if($userp1!=$userp2)
+   {   
+       $error="* Password does not match  "; 
+   }
+   else
+   {
             $sqln="UPDATE user,teacher set   user.full_name='$username', user.password='$userpass1',teacher.tr_phone_no='$phone',user.u_img='$img'
             WHERE user.user_id=teacher.user_id and teacher.tr_phone_no='$phon' ;";
             if(mysqli_query($conn,$sqln)){
