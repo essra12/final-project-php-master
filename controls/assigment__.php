0cd90f2dde03d62c5dae8cfa544aadd85c846a4f
farah-larbah <farahlarbah@gmail.com>
2023-01-11 18:13:24 +0200

    ............

--- controls/assigment__.php
+++ controls/assigment__.php
@@ -1,6 +1,8 @@
 <?php
 include(MAIN_PATH. "/database/db.php");
 
+$errors=array();
+
 $user_id=$_SESSION['user_id'];
 
 ////////////////////////////
@@ -37,12 +39,13 @@ $records=$pre->get_result()->fetch_all(MYSQLI_ASSOC);
 return $records;
 }
 function search($stu_id){ 
+    $group_no=$_SESSION['g_no'];
     $search="";
     global $errors;
     global $conn; 
 
-        $sql="SELECT post.stu_group,user.full_name,user.u_img,student.stu_id FROM post,student_group,announcement,user,student WHERE  post.stu_group=student_group.stu_group AND user.user_id=student.user_id 
-        AND student.stu_id=student_group.stu_id  AND student.stu_id='$stu_id'; ";
+        $sql="SELECT  user.full_name,user.u_img,student.stu_id,post.title,post.Datatime,post.p_no from user,student,student_group,post,file WHERE 
+        user.user_id=student.user_id and student.stu_id=student_group.stu_id and student_group.stu_group=post.stu_group and post.p_no=file.p_no and student_group.g_no='$group_no' AND student.stu_id='$stu_id';";
         $pre=$conn->prepare($sql);
         $pre->execute();
         $exisiting_student_search=$pre->get_result()->fetch_all(MYSQLI_ASSOC);
